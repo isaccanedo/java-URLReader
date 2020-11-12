ed2d28530545379bb7dfd6c5d468ad4b956d8c25
isaccanedo <isaccanedo@gmail.com>
2020-11-12 14:55:25 -0300

    :boom: Initial commit

---
+++ src/br/com/isaccanedo/URLReader.java
@@ -0,0 +1,20 @@
+package br.com.isaccanedo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class URLReader {
+
+    public static void main(String[] args) throws Exception{
+
+        URL google = new URL("https://www.google.com/");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(google.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+    }
+}
